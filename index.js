ad8c3cd8a60f28dec3e6096907c40dd480e9a36d
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-19 01:10:40 -0700

    commit tree bc66e0c3b726e1ee67fbb891a99c1bc7b5cc2159

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // create an object to keep track of all the numbers we've seen
+  const seenNumbers = {};
+  // iterate over the array of numbers
+  for (const number of array) {
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - number;
+    // check if any of the keys in our object is the complement to the current number
+    // if so, return true
+    if (seenNumbers[complement]) return true;
+    // save the current number as the key on our object so we can check it later against other numbers
+    seenNumbers[number] = true;
+  }
+  // if we reach the end of the array, return false
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+   O(N^2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate through each item in the array
+  for the current num, identify a complement that adds uo to the target sum
+  check if any number is our complement
+  if so, return true
+  if i reach the end of the array return false
 */
 
 /*
@@ -29,6 +50,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
